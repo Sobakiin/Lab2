c451d35294f231b089e687155b8cd10eaf3d4ad4
Sobakiin <wgreen16@my.bcit.ca>
2021-01-24 15:50:18 -0800

    Modified to ES6.

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 // Add DOM selectors to target input and UL movie list
-var inp = document.querySelector("input");
-var myMovieList = document.querySelector("ul");
 
 // Example of a simple function that clears the input after a user types something in
-function clearInput() {
+let clearInput = () => {
+    let inp = document.querySelector("input");
     inp.value = "";
 }
 
-function clearMovies() {
+let clearMovies = () => {
     // To delete all children of the <ul></ul> (meaning all <li>'s)..we can wipe out the <ul>'s innerHTML
+    let myMovieList = document.querySelector("ul");
     myMovieList.innerHTML = '';
 }
 
 // This function is executed when the user clicks [ADD MOVIE] button.
-function addMovie() {
+let addMovie = () => {
+    let inp = document.querySelector("input");
+    let myMovieList = document.querySelector("ul");
+
     if(inp.value==""){
         alert("Please enter a movie name")
     }
     else{
-    // Step 1: Get value of input
-    var userTypedText = inp.value;
-    // Step 2: Create an empty <li></li>
-    var li = document.createElement("li"); // <li></li>
+        // Step 1: Get value of input
+        var userTypedText = inp.value;
+        // Step 2: Create an empty <li></li>
+        var li = document.createElement("li"); // <li></li>
 
-    // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
-    var textToInsert = document.createTextNode(userTypedText);
+        // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
+        var textToInsert = document.createTextNode(userTypedText);
 
-    // Step 4: Insert text into li
-    // <li>Harry Potter </li>
-    li.appendChild(textToInsert);
+        // Step 4: Insert text into li
+        // <li>Harry Potter </li>
+        li.appendChild(textToInsert);
 
-    // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
-    myMovieList.appendChild(li);
+        // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
+        myMovieList.appendChild(li);
 
-    // Step 6: Call the clearInput function to clear the input field
-    clearInput();
+        // Step 6: Call the clearInput function to clear the input field
+        clearInput();
     }
 }
