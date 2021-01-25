138868b01eb7b7c3797de5ab4b76babf1782cba6
Sobakiin <wgreen16@my.bcit.ca>
2021-01-24 18:17:45 -0800

    Movie now added to history.

--- app.js
+++ app.js
@@ -24,17 +24,18 @@ let addMovie = () => {
     }
     else{
         // Step 1: Get value of input
-        var userTypedText = inp.value;
+        let userTypedText = inp.value;
         // Step 2: Create an empty <li></li>
-        var li = document.createElement("li"); // <li></li>
+        let li = document.createElement("li"); // <li></li>
 
         // Step 3: Prepare the text we will insert INTO that li ^...example: Harry Potter
-        var textToInsert = document.createTextNode(userTypedText);
+        let textToInsert = document.createTextNode(userTypedText);
 
         // Step 4: Insert text into li
         // <li>Harry Potter </li>
         li.appendChild(textToInsert);
 
+        addHistory()
         // Step 5: Insert the <li>Harry Potter</li> INTO the <ul>
         myMovieList.appendChild(li);
 
@@ -45,4 +46,32 @@ let addMovie = () => {
 
 let addHistory = () => {
     // This function is executed in the previous function to add the movie to the history list.
+    let inp = document.querySelector("input");
+    let myMovieList = document.querySelector("ul");
+    let movieHist = document.querySelector("#movieHistoryCard")//.querySelectorAll("li");
+    let userTypedText = inp.value;
+    let textToInsert = document.createTextNode(userTypedText)
+    //I realize now I could let the relevant nodes be arguments in this function,
+    //Will change that later.
+
+    if(movieHist == null){
+    let movie = document.createElement("il");
+    movie.appendChild(textToInsert);
+    movieHist.appendChild(movie);
+    }
+    else{
+        let presence = false
+        for(let i = 0; i < movieHist.length; i++){
+            
+            if(movieHist[i].contains(textToInsert)){
+                //Some code that adds to the count section
+                presence=true
+            }
+        }
+        if(presence==false){
+            let movie = document.createElement("il");
+            movie.appendChild(textToInsert);
+            movieHist.appendChild(movie)
+        }
+    }
 }
